f52bbb76b26fe70107e201426ea3fe4819f63858
lrosa007 <lucas@10factory.com>
2020-04-08 21:08:24 -0400

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from "react";
+import {render} from "react-dom";
+import {add} from './add.wasm';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(currentCount => add(currentCount, 1));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [setCount]);
+
+
+  return (
+    <div>
+      {count}
+    </div>
+  );
+}
+
+render(
+  <Counter />,
+  document.getElementById("root"),
+);
